0ab7ec8a89d9d11f9f8839b13f9548197f822344
Garrett Davidson <davidson.garrettm@gmail.com>
2018-07-10 18:58:32 -0400

    Read in a loop

--- tagread.cpp
+++ tagread.cpp
@@ -16,19 +16,22 @@ int main(int argc, char **argv) {
   if (device->wakeUp() < 0) { return -1; };
   if (device->setUp() < 0) { return -1; };
 
-  const int idLength = 7;
-  uint8_t idBuffer[idLength];
-  printf("Fetching tag id\n");
-  int receivedIdLength = device->readTagId(idBuffer, idLength, PN532::TypeABaudRate);
+  while (1) {
+    const int idLength = 7;
+    uint8_t idBuffer[idLength];
+    printf("Fetching tag id\n");
+    int receivedIdLength = device->readTagId(idBuffer, idLength, PN532::TypeABaudRate);
 
-  printf("Received id: ");
-  device->printHex(idBuffer, receivedIdLength);
+    printf("Received id: ");
+    device->printHex(idBuffer, receivedIdLength);
 
-  // TODO: Something in nfc-poll is important for SAMConfig to work
-  const int pageSize = 16;
-  uint8_t pageBuffer[pageSize];
-  device->ntag2xxReadPage(1, pageBuffer);
-  device->printHex(pageBuffer, 16);
+    // TODO: Something in nfc-poll is important for SAMConfig to work
+    printf("Fetching page\n");
+    const int pageSize = 16;
+    uint8_t pageBuffer[pageSize];
+    device->ntag2xxReadPage(1, pageBuffer);
+    device->printHex(pageBuffer, 16);
+  }
 
   return 0;
 }
